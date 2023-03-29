ab8b0a028e0dc42ea5ef2ca654a4134a1a2c158b
MuneneCalvin <munenecalvn@gmail.com>
2023-03-29 17:11:31 +0300

    Project

---
+++ publicheader.php
@@ -0,0 +1,12 @@
+<header>
+    <div class="logo">
+        <a href="index.php"><img src="resources/images/rectangularlogo.png"></a>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="index.php" class="hactive">home</a></li>
+            <li><a href="publicservices.php" class="sactive">services</a></li>
+            <li><a class="logout" href="login.php">login</a></li>
+        </ul>
+    </nav>
+</header>
